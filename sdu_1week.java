fc7b0db2e8b55e406522cc3cd169ea4dd8b108d3
Dauletkeldi <146802942+Dauletkeldi@users.noreply.github.com>
2023-11-10 09:41:15 +0600

    Add files via upload

--- sdu_1week.java
+++ sdu_1week.java
@@ -1,15 +1,7 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
 public class sdu_1week {
 
     public static void main(String[] args) {
-        System.out.println("    J     A     V     V     A");
-        System.out.println("    J    A A     V   V     A A");
-        System.out.println("J   J   AAAAA     V V     AAAAA");
-        System.out.println(" J J   A     A     V     A     A");
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
